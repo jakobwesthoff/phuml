f8c1f79a6d69b0fbea639ecf644be13cc3475d40
jakob <jakob@c6656ae0-c128-0410-ba17-c73cf568d732>
2007-07-08 22:33:55 +0000

    - Refactored tokenizerGenerator
    git-svn-id: svn://westhoffswelt.de/phuml@6 c6656ae0-c128-0410-ba17-c73cf568d732

--- src/classes/structure/generator/tokenizer.php
+++ src/classes/structure/generator/tokenizer.php
@@ -2,24 +2,50 @@
 
 class plStructureGeneratorTokenizer implements plStructureGenerator 
 {
+    private $classes;
+    private $interfaces;
+
+    private $class;
+    private $interface;
+    private $variables;
+    private $functions;
+    private $implements;
+    private $extends;
+    private $modifier;
+
+    public function __construct() 
+    {
+        $this->initGlobalAttributes();
+        $this->initParserAttributes();
+    }
+
+    private function initGlobalAttributes() 
+    {
+        $this->classes      = array();
+        $this->interfaces   = array();
+    }
+
+    private function initParserAttributes() 
+    {
+        $this->class        = null;
+        $this->interface    = null;
+        $this->variables    = array();
+        $this->functions    = array();
+        $this->implements   = array();
+        $this->extends      = null;
+        $this->modifier     = 'public';
+        
+        $this->lastToken    = null;
+    }
+
     public function createGraph( array $files ) 
     {
-        $classes    = array();
-        $interfaces = array();
+        $this->initGlobalAttributes();
 
         foreach( $files as $file ) 
         {
-            // Initialize the storage arrays
-            $class      = null;
-            $interface  = null;
-            $variables  = array();
-            $functions  = array();
-            $implements = array();
-            $extends    = null;
-            $modifier   = 'public';
-
+            $this->initParserAttributes();
             $tokens = token_get_all( file_get_contents( $file ) );
-            $lastToken = null;
 
             // Loop through all tokens
             foreach( $tokens as $token ) 
@@ -29,170 +55,152 @@ class plStructureGeneratorTokenizer implements plStructureGenerator
                 {
                     continue;
                 }
-                // New token of interest 
-                else if ( $lastToken === null && is_array( $token ) === true ) 
-                {
-                    switch ( $token[0] ) 
-                    {
-                        case T_CLOSE_TAG:
-                            // First we need to check if we should store the data found so far
-                            if ( $interface !== null ) 
-                            {
-                                // Init data storage
-                                $f = array();
-
-                                // Create the data objects
-                                foreach( $functions as $function ) 
-                                {
-                                    $f[] = new plPhpFunction( $function[0], $function[1] );                                    
-                                }
-                                $i = new plPhpInterface( $interface, $f, $extends );                              
-                                
-                                $interfaces[$interface] = $i;
-                            }
-
-                            // First we need to check if we should store the data found so far
-                            if ( $class !== null ) 
-                            {
-                                // Init data storage
-                                $f = array();
-                                $a = array();                              
-
-                                // Create the data objects
-                                foreach( $functions as $function ) 
-                                {
-                                    $f[] = new plPhpFunction( $function[0], $function[1] );                                    
-                                }
-                                foreach( $variables as $attribute ) 
-                                {
-                                    $a[] = new plPhpAttribute( $attribute[0], $attribute[1] );                                    
-                                }
-                                $c = new plPhpClass( $class, $a, $f, $implements, $extends );                              
-                                
-                                $classes[$class] = $c;
-                            }
-                        break;
-                        case T_INTERFACE:                        
-                            // First we need to check if we should store the data found so far
-                            if ( $interface !== null ) 
-                            {
-                                // Init data storage
-                                $f = array();
-
-                                // Create the data objects
-                                foreach( $functions as $function ) 
-                                {
-                                    $f[] = new plPhpFunction( $function[0], $function[1] );                                    
-                                }
-                                $i = new plPhpInterface( $interface, $f, $extends );                              
-                                
-                                $interfaces[$interface] = $i;
-                            }
-
-                            $class      = null;
-                            $interface  = null;
-                            $variables  = array();
-                            $functions  = array();
-                            $implements = array();
-                            $extends    = null;
-                            $modifier   = 'public';
-
-                            $lastToken = $token[0];
-                        break;
-                        case T_CLASS:
-                            // First we need to check if we should store the data found so far
-                            if ( $class !== null ) 
-                            {
-                                // Init data storage
-                                $f = array();
-                                $a = array();                              
-
-                                // Create the data objects
-                                foreach( $functions as $function ) 
-                                {
-                                    $f[] = new plPhpFunction( $function[0], $function[1] );                                    
-                                }
-                                foreach( $variables as $attribute ) 
-                                {
-                                    $a[] = new plPhpAttribute( $attribute[0], $attribute[1] );                                    
-                                }
-                                $c = new plPhpClass( $class, $a, $f, $implements, $extends );                              
-                                
-                                $classes[$class] = $c;
-                            }
-
-                            $class      = null;
-                            $interface  = null;
-                            $variables  = array();
-                            $functions  = array();
-                            $implements = array();
-                            $extends    = null;
-                            $modifier   = 'public';
-                            
-                            $lastToken = $token[0];
-                        break;
-                        case T_IMPLEMENTS:
-                        case T_EXTENDS:
-                            $lastToken = $token[0];
-                        break;
-                        case T_PUBLIC:
-                        case T_PROTECTED:
-                        case T_PRIVATE:
-                            $modifier  = $token[1];
-                            $lastToken = null;
-                        break;
-                        case T_FUNCTION:
-                            $lastToken = $token[0];
-                        break;
-                    }
-                }
                 // Skip commas as in implements statements
                 else if ( is_array( $token ) !== true && $token === ',' ) 
                 {
                     continue;
                 }
+                // New intial token of interest 
+                else if ( $this->lastToken === null && is_array( $token ) === true ) 
+                {
+                    $this->initialToken( $token );
+                }
                 // We found a string token. Isolate information based on the last token found
                 else if ( is_array( $token ) === true && $token[0] === T_STRING ) 
                 {
-                    switch( $lastToken ) 
-                    {
-                        case T_IMPLEMENTS:
-                            $implements[] = $token[1];
-                        break;
-                        case T_EXTENDS:
-                            $extends = $token[1];
-                            $lastToken = null;
-                        break;                        
-                        case T_FUNCTION:
-                            $functions[] = array( $token[1], $modifier );                           
-                            $lastToken = null;
-                            $modifier = 'public';
-                        break;
-                        case T_CLASS:
-                            $class = $token[1];
-                            $lastToken = null;
-                        break;
-                        case T_INTERFACE:
-                            $interface = $token[1];
-                            $lastToken = null;
-                        break;
-                    }
-
+                    $this->stringToken( $token );
                 } 
+                // There is a new token we do not know and don't want to know
                 else 
                 {
-                    $lastToken = null;
+                    $this->lastToken = null;
                 }
             }
+
+            // Store interface or class in parser arrays
+            $this->storeClassOrInterface();
         }
 
         // Fix the class and interface connections
-        foreach( $classes as $class ) 
+        $this->fixObjectConnections();
+
+        // Return the class and interface structure
+        return array_merge( $this->classes, $this->interfaces );
+    }
+
+    private function initialToken( $token ) 
+    {
+        switch ( $token[0] ) 
+        {
+            case T_INTERFACE:                        
+            case T_CLASS:
+                $this->storeClassOrInterface(); 
+                $this->lastToken = $token[0];
+            break;
+            case T_IMPLEMENTS:
+            case T_EXTENDS:
+                $this->lastToken = $token[0];
+            break;
+            case T_PUBLIC:
+            case T_PROTECTED:
+            case T_PRIVATE:
+                $this->modifier  = $token[1];
+                $this->lastToken = null;
+            break;
+            case T_FUNCTION:
+                $this->lastToken = $token[0];
+            break;
+        }
+    }
+
+    private function stringToken( $token ) 
+    {
+        switch( $this->lastToken ) 
+        {
+            case T_IMPLEMENTS:
+                // Add interface to implements array
+                $this->implements[] = $token[1];
+                // We do not reset the last token here, because there might be multiple interfaces
+            break;
+            case T_EXTENDS:
+                // Set the superclass
+                $this->extends = $token[1];
+                // Reset the last token
+                $this->lastToken = null;
+            break;                        
+            case T_FUNCTION:
+                // Add the current function
+                $this->functions[] = array( $token[1], $this->modifier );                           
+                // Reset the last token
+                $this->lastToken = null;
+                //Reset the modifier state
+                $this->modifier = 'public';
+            break;
+            case T_CLASS:
+                // Set the class name
+                $this->class = $token[1];
+                // Reset the last token
+                $this->lastToken = null;
+            break;
+            case T_INTERFACE:
+                // Set the interface name
+                $this->interface = $token[1];
+                // Reset the last Token
+                $this->lastToken = null;
+            break;
+        }
+    }
+
+    private function storeClassOrInterface() 
+    {
+        // First we need to check if we should store interface data found so far
+        if ( $this->interface !== null ) 
+        {
+            // Init data storage
+            $functions = array();
+
+            // Create the data objects
+            foreach( $this->functions as $function ) 
+            {
+                $functions[] = new plPhpFunction( $function[0], $function[1] );                                    
+            }
+            $interface = new plPhpInterface( $this->interface, $functions, $this->extends );                              
+            
+            $this->interfaces[$this->interface] = $interface;
+        }
+        else if ( $this->class !== null ) 
+        {
+            // Init data storage
+            $functions = array();
+            $variables = array();                              
+
+            // Create the data objects
+            foreach( $this->functions as $function ) 
+            {
+                $functions[] = new plPhpFunction( $function[0], $function[1] );                                    
+            }
+            foreach( $this->variables as $variable ) 
+            {
+                $variables[] = new plPhpAttribute( $variable[0], $variable[1] );                                    
+            }
+            $class = new plPhpClass( $this->class, $variables, $functions, $this->implements, $this->extends );                              
+            
+            $this->classes[$this->class] = $class;
+        }
+
+        $this->initParserAttributes();
+    }
+
+    private function fixObjectConnections() 
+    {
+        foreach( $this->classes as $class ) 
         {
             $implements = array();
             foreach( $class->implements as $key => $impl ) 
             {
-                $implements[$key] = array_key_exists( $impl, $interfaces ) ? $interfaces[$impl]: new plPhpInterface( $impl );
+                $implements[$key] = array_key_exists( $impl, $this->interfaces ) 
+                                    ? $this->interfaces[$impl]
+                                    : new plPhpInterface( $impl );
             }
             $class->implements = $implements;
 
@@ -200,19 +208,20 @@ class plStructureGeneratorTokenizer implements plStructureGenerator
             {
                 continue;
             }
-            $class->extends = array_key_exists( $class->extends, $classes ) ? $classes[$class->extends] : new plPhpClass( $class->extends );
+            $class->extends = array_key_exists( $class->extends, $this->classes ) 
+                              ? $this->classes[$class->extends] 
+                              : new plPhpClass( $class->extends );
         }
-        foreach( $interfaces as $interface ) 
+        foreach( $this->interfaces as $interface ) 
         {           
             if ( $interface->extends === null ) 
             {
                 continue;
             }
-            $interface->extends = array_key_exists( $class->extends, $classes ) ? $classes[$class->extends] : new plPhpClass( $class->extends );
+            $interface->extends = array_key_exists( $interface->extends, $this->interfaces ) 
+                                 ? $this->interfaces[$interface->extends] 
+                                 : new plPhpInterface( $interface->extends );
         }
-
-        // Return the class and interface structure
-        return array_merge( $classes, $interfaces );
     }
 }
 
