d1ff7be07b642ab338ba1afc18655fba5453b6c6
jakob <jakob@c6656ae0-c128-0410-ba17-c73cf568d732>
2007-07-12 23:49:52 +0000

    - Changed dir structure #1
    git-svn-id: svn://westhoffswelt.de/phuml@28 c6656ae0-c128-0410-ba17-c73cf568d732

--- src/autoload/structure_autoload.php
+++ src/autoload/structure_autoload.php
@@ -1,16 +1,16 @@
 <?php
 
 return array( 
-    'plStructureGenerator'                  =>  'interfaces/structure/generator.php',
-    'plStructureTokenizerGenerator'         =>  'classes/structure/generator/tokenizer.php',
+    'plStructureGenerator'                  =>  'interfaces/generator.php',
+    'plStructureTokenizerGenerator'         =>  'classes/generator/tokenizer.php',
 
-    'plStructureWriterNotFoundException'    =>  'exceptions/structure/writer/notFound.php',
+    'plStructureWriterNotFoundException'    =>  'exceptions/writer/notFound.php',
 
-    'plStructureWriter'                     =>  'interfaces/structure/writer.php',
-    'plStructureDotWriter'                  =>  'classes/structure/writer/dot.php',
-    'plStructureDotWriterDefaultStyle'      =>  'classes/structure/writer/dot/style/default.php',
+    'plStructureWriter'                     =>  'interfaces/writer.php',
+    'plStructureDotWriter'                  =>  'classes/writer/dot.php',
+    'plStructureDotWriterDefaultStyle'      =>  'classes/writer/dot/style/default.php',
 
-    'plStructureWriterNotFoundException'    =>  'exceptions/structure/writer/notFound.php',
+    'plStructureWriterNotFoundException'    =>  'exceptions/writer/notFound.php',
 
 );
 
