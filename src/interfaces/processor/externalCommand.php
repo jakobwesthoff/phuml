eb324120c83c0bd3ac76c3ca5560b95ba2f7f63c
jakob <jakob@c6656ae0-c128-0410-ba17-c73cf568d732>
2007-07-19 23:16:47 +0000

    - Added base class for external command processors
    git-svn-id: svn://westhoffswelt.de/phuml@45 c6656ae0-c128-0410-ba17-c73cf568d732

---
+++ src/interfaces/processor/externalCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+abstract class plExternalCommandProcessor extends plProcessor 
+{
+
+    abstract public function getInputTypes();
+    abstract public function getOutputType();
+    abstract public function execute( $infile, $outfile, $type );
+
+    public function process( $input, $type ) 
+    {
+        // Create temporary datafiles
+        $infile  = tempnam( '/tmp', 'phuml' );
+        $outfile = tempnam( '/tmp', 'phuml' );
+        
+        file_put_contents( $infile, $input );
+
+        unlink( $outfile );
+
+        $this->execute( $infile, $outfile, $type );
+        
+        $outdata = file_get_contents( $outfile );
+
+        unlink( $infile );
+        unlink( $outfile );
+
+        return $outdata;
+    }
+}
+
+?>
