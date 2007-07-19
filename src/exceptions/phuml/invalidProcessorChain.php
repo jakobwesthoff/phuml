92250a0e3e79caf79dd58231cb5a42d2793c7b67
jakob <jakob@c6656ae0-c128-0410-ba17-c73cf568d732>
2007-07-19 21:09:45 +0000

    - Corrected syntax error
    git-svn-id: svn://westhoffswelt.de/phuml@39 c6656ae0-c128-0410-ba17-c73cf568d732

--- src/exceptions/phuml/invalidProcessorChain.php
+++ src/exceptions/phuml/invalidProcessorChain.php
@@ -4,7 +4,13 @@ class plPhumlInvalidProcessorChainException extends Exception
 {
     public function __construct( $first, $second ) 
     {
-        parent::__construct( 'To processors in the chain are incompatible. The first processor\'s output is "' . $first . '". The next Processor in the queue does only support the following input types: ' . implode( ', ', $second ) ) . '.' );
+        parent::__construct( 
+            'To processors in the chain are incompatible. The first processor\'s output is "' 
+          . $first 
+          . '". The next Processor in the queue does only support the following input types: ' 
+          . implode( ', ', $second ) 
+          . '.' 
+       );
     }
 }
 
