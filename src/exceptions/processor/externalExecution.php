26baa2a918e09bf05cd3c7d0f53ee8cc5b416cf5
Moritz Hesse <dialgk@gmail.com>
2015-03-25 10:34:38 +0100

    Array handling in Exception

--- src/exceptions/processor/externalExecution.php
+++ src/exceptions/processor/externalExecution.php
@@ -4,6 +4,10 @@ class plProcessorExternalExecutionException extends Exception
 {
     public function __construct( $output ) 
     {
+    	// Convert array to string if is_array
+    	if(is_array($output)) {
+    		$output = var_export($output, true);
+    	}
         parent::__construct( 'Execution of external program failed:' . "\n" . $output );
     }
 }
